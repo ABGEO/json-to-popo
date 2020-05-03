a04384aa4bae50cb6d41a467cf5c571adca81268
ABGEO <takalandzet@gmail.com>
2020-05-03 15:20:06 +0400

    :bulb: Add Copyrights to example files.

--- examples/example.php
+++ examples/example.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the abgeo/json-to-popo.
+ *
+ * Copyright (C) 2020 Temuri Takalandze <takalandzet@gmail.com>.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 use ABGEO\POPO\Example\Person;
 use ABGEO\POPO\Composer;
 
